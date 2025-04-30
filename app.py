fabbbbc405d1ab31b81011f29250deebde25b97f
Parth Dhage <116301483+coderproo@users.noreply.github.com>
2025-04-30 14:17:47 +0530

    Initial commit

--- app.py
+++ app.py
@@ -59,5 +59,15 @@ def predict():
         print(f"Error in predict route: {e}")
         return jsonify({'error': str(e)}), 500
 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Render!"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
